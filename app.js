a6e806b3834a7c12f2a6476072d7f6683e2fb503
unknown <lahjoujiabdessalam@gmai.com>
2022-11-15 01:05:36 +0100

    test

--- app.js
+++ app.js
@@ -207,11 +207,6 @@ app.post("/login", function (req, res) {
 	});
 });
 
-let port = process.env.PORT;
-if (port == null || port == "") {
-	port = 3000;
-}
-
-app.listen(port, function () {
+app.listen(process.env.PORT, function () {
 	console.log("Server started");
 });
